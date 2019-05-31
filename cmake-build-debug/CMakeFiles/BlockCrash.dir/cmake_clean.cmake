1b3cb01a7d69e2d9d84d038793ddee5c114fd5df
birdy <birdy.xy97@gmail.com>
2019-05-31 09:53:34 +0800

    init repo

---
+++ cmake-build-debug/CMakeFiles/BlockCrash.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BlockCrash.dir/main.cpp.o"
+  "BlockCrash.pdb"
+  "BlockCrash"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlockCrash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
