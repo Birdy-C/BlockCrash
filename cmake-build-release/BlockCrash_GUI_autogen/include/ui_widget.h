1bb3e1cece9e43eba0a5aa8e0426365a9d383159
birdy <birdy.xy97@gmail.com>
2019-06-01 14:17:49 +0800

    add release

---
+++ cmake-build-release/BlockCrash_GUI_autogen/include/ui_widget.h
@@ -0,0 +1,209 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_9;
+    QPushButton *pushButton_10;
+    QPushButton *pushButton_11;
+    QPushButton *pushButton_12;
+    QPushButton *pushButton_13;
+    QPushButton *pushButton_14;
+    QPushButton *pushButton_15;
+    QPushButton *pushButton_16;
+    QPushButton *pushButton_17;
+    QPushButton *pushButton_18;
+    QPushButton *pushButton_19;
+    QPushButton *pushButton_20;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(839, 775);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(620, 90, 151, 61));
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(200, 0, 400, 400));
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(70, 420, 700, 300));
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(620, 170, 131, 91));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(80, 440, 31, 31));
+        pushButton->setFlat(true);
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(130, 440, 41, 31));
+        pushButton_2->setFlat(true);
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(120, 480, 41, 51));
+        pushButton_3->setFlat(true);
+        pushButton_4 = new QPushButton(centralWidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(190, 440, 61, 31));
+        pushButton_4->setFlat(true);
+        pushButton_5 = new QPushButton(centralWidget);
+        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
+        pushButton_5->setGeometry(QRect(190, 490, 31, 71));
+        pushButton_5->setFlat(true);
+        pushButton_6 = new QPushButton(centralWidget);
+        pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
+        pushButton_6->setGeometry(QRect(270, 440, 41, 41));
+        pushButton_6->setFlat(true);
+        pushButton_7 = new QPushButton(centralWidget);
+        pushButton_7->setObjectName(QString::fromUtf8("pushButton_7"));
+        pushButton_7->setGeometry(QRect(270, 490, 41, 41));
+        pushButton_7->setFlat(true);
+        pushButton_8 = new QPushButton(centralWidget);
+        pushButton_8->setObjectName(QString::fromUtf8("pushButton_8"));
+        pushButton_8->setGeometry(QRect(270, 540, 41, 41));
+        pushButton_8->setFlat(true);
+        pushButton_9 = new QPushButton(centralWidget);
+        pushButton_9->setObjectName(QString::fromUtf8("pushButton_9"));
+        pushButton_9->setGeometry(QRect(270, 590, 41, 41));
+        pushButton_9->setFlat(true);
+        pushButton_10 = new QPushButton(centralWidget);
+        pushButton_10->setObjectName(QString::fromUtf8("pushButton_10"));
+        pushButton_10->setGeometry(QRect(330, 440, 41, 41));
+        pushButton_10->setFlat(true);
+        pushButton_11 = new QPushButton(centralWidget);
+        pushButton_11->setObjectName(QString::fromUtf8("pushButton_11"));
+        pushButton_11->setGeometry(QRect(390, 440, 101, 31));
+        pushButton_11->setFlat(true);
+        pushButton_12 = new QPushButton(centralWidget);
+        pushButton_12->setObjectName(QString::fromUtf8("pushButton_12"));
+        pushButton_12->setGeometry(QRect(390, 480, 41, 91));
+        pushButton_12->setFlat(true);
+        pushButton_13 = new QPushButton(centralWidget);
+        pushButton_13->setObjectName(QString::fromUtf8("pushButton_13"));
+        pushButton_13->setGeometry(QRect(490, 440, 61, 61));
+        pushButton_13->setFlat(true);
+        pushButton_14 = new QPushButton(centralWidget);
+        pushButton_14->setObjectName(QString::fromUtf8("pushButton_14"));
+        pushButton_14->setGeometry(QRect(490, 500, 61, 61));
+        pushButton_14->setFlat(true);
+        pushButton_15 = new QPushButton(centralWidget);
+        pushButton_15->setObjectName(QString::fromUtf8("pushButton_15"));
+        pushButton_15->setGeometry(QRect(490, 570, 61, 61));
+        pushButton_15->setFlat(true);
+        pushButton_16 = new QPushButton(centralWidget);
+        pushButton_16->setObjectName(QString::fromUtf8("pushButton_16"));
+        pushButton_16->setGeometry(QRect(490, 640, 61, 61));
+        pushButton_16->setFlat(true);
+        pushButton_17 = new QPushButton(centralWidget);
+        pushButton_17->setObjectName(QString::fromUtf8("pushButton_17"));
+        pushButton_17->setGeometry(QRect(580, 440, 61, 61));
+        pushButton_17->setFlat(true);
+        pushButton_18 = new QPushButton(centralWidget);
+        pushButton_18->setObjectName(QString::fromUtf8("pushButton_18"));
+        pushButton_18->setGeometry(QRect(660, 440, 101, 31));
+        pushButton_18->setFlat(true);
+        pushButton_19 = new QPushButton(centralWidget);
+        pushButton_19->setObjectName(QString::fromUtf8("pushButton_19"));
+        pushButton_19->setGeometry(QRect(650, 480, 41, 111));
+        pushButton_19->setFlat(true);
+        pushButton_20 = new QPushButton(centralWidget);
+        pushButton_20->setObjectName(QString::fromUtf8("pushButton_20"));
+        pushButton_20->setGeometry(QRect(610, 330, 61, 61));
+        pushButton_20->setFlat(false);
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 839, 22));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QApplication::translate("MainWindow", "\345\223\210\345\223\210\357\274\214Clion+Qt\351\205\215\347\275\256\346\210\220\345\212\237\357\274\201", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "TextLabel", nullptr));
+        label_3->setText(QApplication::translate("MainWindow", "TextLabel", nullptr));
+        label_4->setText(QApplication::translate("MainWindow", "TextLabel", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "1", nullptr));
+        pushButton_2->setText(QApplication::translate("MainWindow", "2", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "3", nullptr));
+        pushButton_4->setText(QApplication::translate("MainWindow", "4", nullptr));
+        pushButton_5->setText(QApplication::translate("MainWindow", "5", nullptr));
+        pushButton_6->setText(QApplication::translate("MainWindow", "6", nullptr));
+        pushButton_7->setText(QApplication::translate("MainWindow", "7", nullptr));
+        pushButton_8->setText(QApplication::translate("MainWindow", "8", nullptr));
+        pushButton_9->setText(QApplication::translate("MainWindow", "9", nullptr));
+        pushButton_10->setText(QApplication::translate("MainWindow", "10", nullptr));
+        pushButton_11->setText(QApplication::translate("MainWindow", "11", nullptr));
+        pushButton_12->setText(QApplication::translate("MainWindow", "12", nullptr));
+        pushButton_13->setText(QApplication::translate("MainWindow", "13", nullptr));
+        pushButton_14->setText(QApplication::translate("MainWindow", "14", nullptr));
+        pushButton_15->setText(QApplication::translate("MainWindow", "15", nullptr));
+        pushButton_16->setText(QApplication::translate("MainWindow", "16", nullptr));
+        pushButton_17->setText(QApplication::translate("MainWindow", "17", nullptr));
+        pushButton_18->setText(QApplication::translate("MainWindow", "18", nullptr));
+        pushButton_19->setText(QApplication::translate("MainWindow", "19", nullptr));
+        pushButton_20->setText(QApplication::translate("MainWindow", "\346\222\244\351\224\200", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
